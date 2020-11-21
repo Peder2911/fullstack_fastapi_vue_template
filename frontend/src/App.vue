62a52759d84ad3d6003754aa3aeab512f0410e00
peder2911 <pglandsverk@gmail.com>
2020-11-21 18:54:07 +0100

    Fixed CSS (had to link to app.css obviously). Now works perfectly!

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,15 @@
 <template>
    <div>Hello from Vue {{value }}</div>
 </template>
-<style></style>
-<script>
+<style lang="sass">
+div
+   height: 100%
+   width: 100%
+   display: grid
+   place-items: center
+   font-size: 60px
+</style>
+<script lang="ts">
 export default {
    name: "App",
    data(){
