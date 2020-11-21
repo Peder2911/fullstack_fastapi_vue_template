01167486c422b0f848a1d36039f2acdc12a63aea
peder2911 <pglandsverk@gmail.com>
2020-11-21 17:52:35 +0100

    initial

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+   <div>Hello from Vue {{value }}</div>
+</template>
+<style></style>
+<script>
+export default {
+   name: "App",
+   data(){
+      return {
+         value : 1
+      }
+   }
+}
+</script>
